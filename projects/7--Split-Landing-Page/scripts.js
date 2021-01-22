97fa40869ec4e3a2acd85e2c8ada55d20691d457
John McKie <john.mckie@remarkable.net>
2021-01-22 20:26:56 +0000

    start of something ace

--- projects/7--Split-Landing-Page/scripts.js
+++ projects/7--Split-Landing-Page/scripts.js
@@ -1,9 +1,36 @@
 const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 const container = document.querySelector('.container');
+const leftButton = document.querySelector('.left .btn');
+const rightButton = document.querySelector('.right .btn');
+
 
 left.addEventListener('mouseenter', () => container.classList.add('hover-left'));
 left.addEventListener('mouseleave', () => container.classList.remove('hover-left'));
 
 right.addEventListener('mouseenter', () => container.classList.add('hover-right'));
 right.addEventListener('mouseleave', () => container.classList.remove('hover-right'));
+
+
+// apply animation to the page before navigating to the new one
+leftButton.addEventListener('click', (e) => {
+    left.classList.add('fully-open');
+    right.classList.add('closed');
+
+    setTimeout(() => {
+        left.classList.add('loader');
+    }, 1000);
+
+});
+
+// split the timeouts into a single function for efficiency
+
+rightButton.addEventListener('click', (e) => {
+    right.classList.add('fully-open');
+    left.classList.add('closed');
+
+    setTimeout(() => {
+        right.classList.add('loader');
+    }, 1000);
+
+});
